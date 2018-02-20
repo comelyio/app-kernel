8cd9a7e325e2948a5cc6ae5f645a8caeb61f937f
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-20 06:23:02 +0500

    HTTP controller

---
+++ src/AppKernel/Http/Controller.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of Comely App Kernel package.
+ * https://github.com/comelyio/app-kernel
+ *
+ * Copyright (c) 2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/app-kernel/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\AppKernel\Http;
+
+use Comely\AppKernel;
+
+/**
+ * Class Controller
+ * @package Comely\AppKernel\Http
+ */
+class Controller extends \Comely\IO\HttpRouter\Controller
+{
+    /** @var AppKernel */
+    protected $app;
+
+    /**
+     * @throws AppKernel\Exception\AppKernelException
+     */
+    public function callback(): void
+    {
+        $this->app = AppKernel::getInstance();
+    }
+}
