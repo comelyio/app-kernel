eb4cc10bd933fe450fd4758881a6995f3d95ff24
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-20 05:07:10 +0500

    Exceptions

---
+++ src/Exception/ServicesException.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of Comely Framework Kernel package.
+ * https://github.com/comelyio/framework-kernel
+ *
+ * Copyright (c) 2016-2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/framework-kernel/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\AppKernel\Exception;
+
+/**
+ * Class ServicesException
+ * @package Comely\AppKernel\Exception
+ */
+class ServicesException extends AppKernelException
+{
+    /**
+     * @param string $service
+     * @param string $message
+     * @return ServicesException
+     */
+    public static function ServiceError(string $service, string $message): self
+    {
+        return new self(sprintf('Service [%s] error: %s', $service, $message));
+    }
+}
