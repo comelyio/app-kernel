f7db4e1ed9d42e4ecef10d0c4a3ec8d363615b46
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-20 05:41:04 +0500

    Exceptions

--- src/AppKernel/Exception/MemoryException.php
+++ src/AppKernel/Exception/MemoryException.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * This file is part of Comely App Kernel package.
- *  https://github.com/comelyio/app-kernel
+ * https://github.com/comelyio/app-kernel
  *
- *  Copyright (c) 2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
+ * Copyright (c) 2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *  
  * For the full copyright and license information, please view the LICENSE
- *  file that was distributed with this source code or visit following link:
- *  https://github.com/comelyio/app-kernel/blob/master/LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/app-kernel/blob/master/LICENSE
  */
 
 declare(strict_types=1);
