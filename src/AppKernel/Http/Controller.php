d4a0d1d3224b1ed616e8098002b751fb95be0b5b
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-21 14:57:34 +0500

    requested controller method

--- src/AppKernel/Http/Controller.php
+++ src/AppKernel/Http/Controller.php
@@ -15,10 +15,13 @@ declare(strict_types=1);
 namespace Comely\AppKernel\Http;
 
 use Comely\AppKernel;
+use Comely\Kernel\Comely;
 
 /**
  * Class Controller
  * @package Comely\AppKernel\Http
+ * @method void start()
+ * @method void finish()
  */
 abstract class Controller extends \Comely\IO\HttpRouter\Controller
 {
@@ -26,14 +29,54 @@ abstract class Controller extends \Comely\IO\HttpRouter\Controller
     protected $app;
 
     /**
-     * @throws AppKernel\Exception\AppKernelException
+     * @throws AppKernel\Exception\BootstrapException
+     * @throws ControllerException
      */
     public function callback(): void
     {
         // Set AppKernel instance
         $this->app = AppKernel::getInstance();
 
-        var_dump(get_called_class());
-        var_dump($this->request()->uri());
+        // Start controller execution callback
+        if (method_exists($this, "start")) {
+            $this->start();
+        }
+
+        // Controller method
+        $controllerMethod = strtolower($this->request()->method());
+
+        // Explicit method name
+        $requestedControllerMethod = explode("&", $this->request()->_queryString)[0];
+        if (preg_match('/^[a-z0-9\_]+$/i', $requestedControllerMethod)) {
+            $controllerMethod .= Comely::PascalCase($requestedControllerMethod);
+        }
+
+        if (!method_exists($this, $controllerMethod)) {
+            throw new ControllerException(
+                sprintf(
+                    'Requested method "%s" not found in HTTP controller "%s" class',
+                    $controllerMethod,
+                    get_called_class()
+                )
+            );
+        }
+
+        // Execute
+        try {
+            call_user_func([$this, $controllerMethod]);
+        } catch (ControllerException $e) {
+            $this->error($e->getMessage(), $e->getCode());
+        }
+
+        // Finish controller execution callback
+        if (method_exists($this, "finish")) {
+            $this->finish();
+        }
     }
+
+    /**
+     * @param string $message
+     * @param int $code
+     */
+    abstract public function error(string $message, int $code): void;
 }
