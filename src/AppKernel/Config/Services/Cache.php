f3060a7182461e943ddbab9bf2293d57e59bef38
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-03-01 14:30:17 +0500

    time_out param

--- src/AppKernel/Config/Services/Cache.php
+++ src/AppKernel/Config/Services/Cache.php
@@ -35,6 +35,8 @@ class Cache extends AbstractConfigNode
     protected $port;
     /** @var bool */
     protected $terminate;
+    /** @var int */
+    protected $timeOut;
 
     /**
      * Cache constructor.
@@ -61,11 +63,19 @@ class Cache extends AbstractConfigNode
 
         // Port
         $port = $options["port"] ?? null;
-        if (!is_int($port)) {
+        if (!is_int($port) || $port <= 0) {
             throw ConfigException::PropError('services.cache', 'Property "port" must be of type integer');
         }
 
         $this->port = $port;
+
+        // Time out
+        $timeOut = $options["time_out"] ?? $options["timeOut"] ?? $options["timeout"] ?? null;
+        if (!is_int($timeOut) || $timeOut <= 0) {
+            throw ConfigException::PropError('services.cache', 'Property "timeout" must be a positive integer');
+        }
+
+        $this->timeOut = $timeOut;
 
         // Terminate?
         $terminate = $options["terminate"] ?? null;
